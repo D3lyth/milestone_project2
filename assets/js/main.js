6c4cc6b168434d929c4093f075b8e362d28d3b7c
Del <d3lyth@yahoo.co.uk>
2023-06-20 20:33:53 +0000

    Add variables for guessing function

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+let height = 6; // Number of guesses
+let width = 5; //Number of letters/emojis
+//Players guessing position
+let row = 0; //current guess attempt
+let col = 0; //current letter within attempt
+let gameOver = false;
+let word;
+let testAnswer = document.getElementById("test_answer");
